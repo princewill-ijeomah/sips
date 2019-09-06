c67c8a1b25e95725ba57f371cf10f11e02bb78c7
havizIM <viz.ndinq@gmail.com>
2019-09-06 13:39:05 +0100

    Update Rest Config

--- application/config/rest.php
+++ application/config/rest.php
@@ -110,7 +110,7 @@ $config['rest_realm'] = 'REST API';
 |           authorization key
 |
 */
-$config['rest_auth'] = false;
+$config['rest_auth'] = 'basic';
 
 /*
 |--------------------------------------------------------------------------
@@ -211,7 +211,7 @@ $config['auth_library_function'] = '';
 | Array of usernames and passwords for login, if ldap is configured this is ignored
 |
 */
-$config['rest_valid_logins'] = ['admin' => '1234'];
+$config['rest_valid_logins'] = ['sips-codemaniac' => 'codemaniac123'];
 
 /*
 |--------------------------------------------------------------------------
@@ -377,7 +377,7 @@ $config['rest_key_length'] = 40;
 | 2012/06/12. See RFC 6648 specification for more details
 |
 */
-$config['rest_key_name'] = 'X-API-KEY';
+$config['rest_key_name'] = 'SIPS-KEY';
 
 /*
 |--------------------------------------------------------------------------
@@ -534,7 +534,7 @@ $config['rest_ajax_only'] = false;
 | Language file to load from the language directory
 |
 */
-$config['rest_language'] = 'english';
+$config['rest_language'] = 'indonesia';
 
 /*
 |--------------------------------------------------------------------------
@@ -561,7 +561,9 @@ $config['allowed_cors_headers'] = [
   'X-Requested-With',
   'Content-Type',
   'Accept',
-  'Access-Control-Request-Method'
+  'Access-Control-Request-Method',
+  'SIPS-KEY',
+  'Authorization'
 ];
 
 /*
