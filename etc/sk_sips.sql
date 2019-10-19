54e16c4d572e7b9bbaaad2e2382db6ec12d3d1cc
havizIM <viz.ndinq@gmail.com>
2019-10-19 20:36:13 +0700

    Update Haviz

--- etc/sk_sips.sql
+++ etc/sk_sips.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Waktu pembuatan: 18 Okt 2019 pada 02.28
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.1.32
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Okt 2019 pada 15.35
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -47,7 +47,8 @@ CREATE TABLE `konfirmasi` (
 --
 
 INSERT INTO `konfirmasi` (`no_konfirmasi`, `no_transaksi`, `bank`, `bank_pengirim`, `rekening_pengirim`, `nama_pengirim`, `tgl_transfer`, `jml_transfer`, `foto`, `tgl_input`, `valid`) VALUES
-('KNF-0000001', 'TRX-0000001', '123123123', 'BCA', '123123123', 'Hadji', '2019-10-07', 75000, 'test.jpg', '2019-10-07 08:37:21', 'Y');
+('KNF-0000001', 'TRX-0000001', '123123123', 'BCA', '123123123', 'Hadji', '2019-10-07', 75000, 'test.jpg', '2019-10-07 08:37:21', 'Y'),
+('KNF-0000002', 'TRX-0000002', 'BCA -1791606298', 'BCA', '123123', 'Siapa aja', '2019-10-19', 200000, 'test.jpg', '2019-10-19 13:26:22', 'Y');
 
 -- --------------------------------------------------------
 
@@ -66,7 +67,8 @@ CREATE TABLE `kriteria` (
 
 INSERT INTO `kriteria` (`id_kriteria`, `nama_kriteria`) VALUES
 (12, 'Cobaaaaa'),
-(13, 'Yeeeaaahhhh');
+(13, 'Yeeeaaahhhh'),
+(15, 'Harga');
 
 -- --------------------------------------------------------
 
@@ -88,7 +90,8 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id_product`, `nama_product`, `weight`, `harga`, `deskripsi`, `foto`) VALUES
-('PR-00000001', 'Cobaaa', 123, 25000, 'Test', 'test.jpg');
+('PR-00000001', 'Cobaaa', 123, 25000, 'Test', 'PR-00000001.png'),
+('PR-00000002', 'Cairan Kimia', 1200, 130000, 'Ini adalah pembesar segalanya\r\n\r\nnamanya juga umur mah ga ada yang tau', 'PR-00000002.PNG');
 
 -- --------------------------------------------------------
 
@@ -106,8 +109,12 @@ CREATE TABLE `product_subkriteria` (
 --
 
 INSERT INTO `product_subkriteria` (`id_product`, `id_subkriteria`) VALUES
-('PR-00000001', 15),
-('PR-00000001', 10);
+('PR-00000002', 19),
+('PR-00000002', 10),
+('PR-00000002', 15),
+('PR-00000001', 18),
+('PR-00000001', 10),
+('PR-00000001', 15);
 
 -- --------------------------------------------------------
 
@@ -129,7 +136,9 @@ INSERT INTO `subkriteria` (`id_subkriteria`, `id_kriteria`, `nama_subkriteria`) 
 (9, 13, 'Cobaaaa'),
 (10, 13, 'Yaaaa'),
 (14, 12, 'Cobaaa'),
-(15, 12, 'Bahaya Banget');
+(15, 12, 'Bahaya Banget'),
+(18, 15, '> 1.000.000'),
+(19, 15, '< 1.000.000');
 
 -- --------------------------------------------------------
 
@@ -141,6 +150,7 @@ CREATE TABLE `transaksi` (
   `no_transaksi` varchar(11) NOT NULL,
   `id_user` varchar(11) NOT NULL,
   `alamat_kirim` text NOT NULL,
+  `total` int(11) NOT NULL,
   `status` enum('Belum Dibayar','Dibayar') NOT NULL,
   `tgl_transaksi` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -149,8 +159,9 @@ CREATE TABLE `transaksi` (
 -- Dumping data untuk tabel `transaksi`
 --
 
-INSERT INTO `transaksi` (`no_transaksi`, `id_user`, `alamat_kirim`, `status`, `tgl_transaksi`) VALUES
-('TRX-0000001', 'USR-0000002', 'Jl. Jakarta yaaaaa', 'Dibayar', '2019-10-07 08:37:21');
+INSERT INTO `transaksi` (`no_transaksi`, `id_user`, `alamat_kirim`, `total`, `status`, `tgl_transaksi`) VALUES
+('TRX-0000001', 'USR-0000002', 'Jl. Jakarta yaaaaa', 200000, 'Belum Dibayar', '2019-10-19 13:23:21'),
+('TRX-0000002', 'USR-0000005', 'Jakarta', 200000, 'Dibayar', '2019-10-19 13:21:02');
 
 -- --------------------------------------------------------
 
@@ -172,7 +183,9 @@ CREATE TABLE `transaksi_detail` (
 
 INSERT INTO `transaksi_detail` (`no_transaksi`, `id_product`, `harga_satuan`, `qty`, `total_harga`) VALUES
 ('TRX-0000001', 'PR-00000001', 25000, 2, 50000),
-('TRX-0000001', 'PR-00000001', 30000, 5, 150000);
+('TRX-0000001', 'PR-00000001', 30000, 5, 150000),
+('TRX-0000002', 'PR-00000001', 100000, 1, 100000),
+('TRX-0000002', 'PR-00000002', 100000, 1, 100000);
 
 -- --------------------------------------------------------
 
@@ -200,9 +213,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama_lengkap`, `jenis_kelamin`, `tgl_lahir`, `alamat`, `telepon`, `email`, `username`, `password`, `aktif`, `level`, `tgl_registrasi`) VALUES
-('USR-0000001', 'Vicky Kurnia', 'P', '2019-09-25', 'Jakarta Barat', '08987748441', 'viz.ndinq@gmail.com', 'vicky', 'e79cab55eab4c0a1a63610829a51fd51d5cfb294', 'Y', 'Owner', '2019-09-25 07:57:15'),
-('USR-0000002', 'Haviz Indra Maulana', 'L', '1992-10-10', 'Jakarta', '08987748441', 'haviz_im@outlook.com', 'havizIM', '9a64164ce3c1429c5854559980c24b84634f5681', 'Y', 'Customer', '2019-09-25 11:22:05'),
-('USR-0000003', 'Dian Ratna Sari Maulana', 'P', '1992-10-10', 'Tanah Sereal, Tambora, Jakarta Barat', '08987748441', 'dianrs19@gmail.com', 'dianrs', 'e2baa50d717f2e8d7e04c8bdc5d22ba737d01376', 'Y', 'Cashier', '2019-10-02 22:47:45');
+('USR-0000001', 'Vicky Kurnia', 'P', '2019-09-25', 'Jakarta Barat', '08987748441', 'viz.ndinq@gmail.com', 'vicky', 'e79cab55eab4c0a1a63610829a51fd51d5cfb294', 'Y', 'Owner', '2019-10-18 01:56:50'),
+('USR-0000002', 'Haviz Indra Maulana', 'L', '1992-10-10', 'Jakarta', '08987748441', 'haviz_im@outlook.com', 'havizIM', '37403280b6e8573ea86d801a32419ef93d06a52e', 'Y', 'Customer', '2019-10-19 10:52:05'),
+('USR-0000003', 'Haviz Indra Maulana', 'L', '1992-10-10', 'Jakarta Barat', '08987748441', 'haviz_im@gmail.com', 'haviz_im', '9cb559c8b811362e7657090091052e8542176009', 'Y', 'Cashier', '2019-10-18 02:42:12'),
+('USR-0000004', 'Devan DP', 'L', '1992-10-10', 'Jakarta', '123123', 'devandp@gmail.com', 'devandp', 'cbb527c93eb9b5289339e046dfca4af65d7b9608', 'Y', 'Customer', '2019-10-19 12:17:35'),
+('USR-0000005', 'Kalyssa Innara Putri', 'P', '1992-10-10', 'Jakarta', '123123', 'kalyssa@gmail.com', 'kalyssa', '971c814078d430b6ca6cd35674ba22f35ead77b4', 'T', 'Cashier', '2019-10-19 13:16:28');
 
 --
 -- Indexes for dumped tables
@@ -269,13 +284,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id_kriteria` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_kriteria` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `subkriteria`
 --
 ALTER TABLE `subkriteria`
-  MODIFY `id_subkriteria` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_subkriteria` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
